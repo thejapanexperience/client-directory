218235d2bfd8ca063e86045ae45eacdc51948923
Richard Mands <richard@eduku.org>
2016-10-31 12:34:01 -0700

    removed redundant frontent

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
-import Navbar from './Navbar';
-import Landing from './Landing';
 
 export default class Layout extends Component {
   render() {
     return (
 
-      <div>
-        <Navbar />
-        <div id="main">{this.props.children}</div>
-      </div>
+      <div />
     );
   }
 }
